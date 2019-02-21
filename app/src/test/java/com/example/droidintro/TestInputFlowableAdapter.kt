cbd6d00fce438de293eca1c7f6d4e9240dc4aafb
Vadzim Vysotski <v.vysotski@godeltech.com>
2019-02-21 11:56:15 +0300

    test big buffer

--- app/src/test/java/com/example/droidintro/TestInputFlowableAdapter.kt
+++ app/src/test/java/com/example/droidintro/TestInputFlowableAdapter.kt
@@ -9,7 +9,7 @@ import org.junit.Assert.*
 
 class TestInputFlowableAdapter {
     @Test
-    fun testInputToFlowable() {
+    fun testInputToFlowableSmallBuffer() {
         //arrange
         val input = inputFromText("testdata x")
         val testSubscriber = TestSubscriber<String>()
@@ -22,5 +22,21 @@ class TestInputFlowableAdapter {
         testSubscriber.assertNoErrors()
         testSubscriber.assertValueCount(3)
         assertArrayEquals(testSubscriber.values().toTypedArray(), arrayOf("test", "data", " x"))
+    }
+
+    @Test
+    fun testInputToFlowableBufferBiggerThenText() {
+        //arrange
+        val input = inputFromText("testdata x")
+        val testSubscriber = TestSubscriber<String>()
+
+        //act
+        textInputToFlowable(input, 500).subscribe(testSubscriber)
+
+        //assert
+        testSubscriber.assertComplete()
+        testSubscriber.assertNoErrors()
+        testSubscriber.assertValueCount(1)
+        assertArrayEquals(testSubscriber.values().toTypedArray(), arrayOf("testdata x"))
     }
 }
